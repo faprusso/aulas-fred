048fd60938d0c42f675a2a258e2ee615603854e1
faprusso <faprusso@gmail.com>
2023-05-02 08:36:30 -0300

    Primeiro Commit

---
+++ aula01/app.js
@@ -0,0 +1,69 @@
+
+// variáveis
+// var, let e const
+
+// nunca utilizar símbolos
+// nunca utilizar números no começo
+// nunca utilizar espaço
+// utilizar o padrão camelCase
+
+// tipagem fraca e dinâmica
+
+// tipos de dados
+
+// number
+let dado = 0;
+console.log(typeof dado);
+
+dado = "Um texto qualquer";
+// console.log(dado);
+console.log(typeof dado);
+
+// true/false
+dado = false;
+console.log(typeof dado);
+
+dado = [3, 12, "asdf", true, []];
+console.log(typeof dado);
+
+dado = {
+  nome: "Maria",
+  idade: 43,
+  endereco: {
+    cidade: "Belém",
+    estado: "Pará",
+  },
+  hobbies: ["Futebol", "Cinema"],
+}
+console.log(typeof dado);
+
+let info = "texto";
+console.log(info);
+
+// operadores lógicos
+// E / AND - &&
+// OU / OR - ||
+// NÃO / NOT - !
+
+let numero = 14;
+
+// numero > 0
+// numero < 10
+
+// console.log(numero > 0 && numero < 10);
+
+// console.log(numero > 0 || numero < 10);
+
+console.log(!false);
+
+
+// operadores relacionais
+// operadores matemáticos
+
+
+// estruturas de controle
+// let idade = 18;
+// console.log(idade > 12);
+// if (idade > 18) {
+//   console.log("Maior de idade");
+// }
